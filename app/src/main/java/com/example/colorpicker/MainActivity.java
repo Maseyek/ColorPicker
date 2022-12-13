d30f05dd5588867023a68b970bfcba1cbcc7212a
Jakub Król <maseyalke@gmail.com>
2022-12-13 13:30:44 +0100

    Mark clicked spots

--- app/src/main/java/com/example/colorpicker/MainActivity.java
+++ app/src/main/java/com/example/colorpicker/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.colorpicker;
 
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
@@ -13,6 +16,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -41,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
     private static final int CAMERA_REQUEST = 1888;
     private static final int MY_CAMERA_PERMISSION_CODE = 100;
     public int precision = 5, evX, evY;
+    Bitmap bitmap_Temp, bitmap_Calc;
+    boolean isFresh;
 
 
 
@@ -97,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
                 {
                     for(int j = -(precision/2); j < (precision/2); j++)
                     {
-                        int touchColor = getColor(uploadedImage, evX + i, evY + j);
+                        int touchColor = getColor(bitmap_Calc, evX + i, evY + j);
 
                         r[count] = (touchColor >> 16) & 0xFF;
                         g[count] = (touchColor >> 8) & 0xFF;
@@ -138,12 +144,25 @@ public class MainActivity extends AppCompatActivity {
                     final int action = motionEvent.getAction();
                     evX = (int) motionEvent.getX();
                     evY = (int) motionEvent.getY();
+                    if (action==MotionEvent.ACTION_DOWN)
+                    {
+                        if(isFresh == TRUE) {
+                            uploadedImage.setDrawingCacheEnabled(true);
+                            bitmap_Temp = Bitmap.createBitmap(uploadedImage.getDrawingCache());
+                            bitmap_Calc = Bitmap.createBitmap(uploadedImage.getDrawingCache());
+                            uploadedImage.setDrawingCacheEnabled(false);
+                            isFresh = FALSE;
+                        }
+                        bitmap_Temp = bitmap_Calc.copy(bitmap_Calc.getConfig(), true);
+                        uploadedImage.setImageBitmap(bitmap_Calc);
+
+                    }
 
 
 
 
 
-                    int touchColor = getColor(uploadedImage, evX, evY);
+                    int touchColor = getColor(bitmap_Calc, evX, evY);
 
                     int r = (touchColor >> 16) & 0xFF;
                     int g = (touchColor >> 8) & 0xFF;
@@ -153,7 +172,15 @@ public class MainActivity extends AppCompatActivity {
 
                     if (action==MotionEvent.ACTION_UP)
                     {
-                        colorDisplay.setBackgroundColor(getColor(uploadedImage, evX , evY));
+                        colorDisplay.setBackgroundColor(getColor(bitmap_Calc, evX , evY));
+                        for (int i = -(precision/2); i < (precision/2); i++) {
+                            for (int j = -(precision/2); j < (precision/2); j++) {
+                                bitmap_Temp.setPixel(evX + i, evY + j, Color.BLACK);
+                            }
+                        }
+
+                        uploadedImage.setImageBitmap(bitmap_Temp);
+
                     }
                 }catch (Exception e){}
 
@@ -206,13 +233,20 @@ public class MainActivity extends AppCompatActivity {
             precision = p;
     }
 
-    private int getColor(ImageView uploadedImage, int evX, int evY){
-        uploadedImage.setDrawingCacheEnabled(true);
-        Bitmap bitmap = Bitmap.createBitmap(uploadedImage.getDrawingCache());
-        uploadedImage.setDrawingCacheEnabled(false);
+//    private int getColor(ImageView uploadedImage, int evX, int evY){
+//        uploadedImage.setDrawingCacheEnabled(true);
+//        Bitmap bitmap = Bitmap.createBitmap(uploadedImage.getDrawingCache());
+//        uploadedImage.setDrawingCacheEnabled(false);
+//        return bitmap.getPixel(evX, evY);
+//
+//    }
+
+    private int getColor(Bitmap bitmap, int evX, int evY){
+
         return bitmap.getPixel(evX, evY);
 
     }
+
 
 
     @Override
@@ -221,11 +255,12 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == 1 && resultCode == RESULT_OK && !data.equals(null)){
             Uri Image = data.getData();
             uploadedImage.setImageURI(Image);
-
+            isFresh = TRUE;
         }
         else if(requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK){
             Bitmap photo = (Bitmap)data.getExtras().get("data");
             uploadedImage.setImageBitmap(photo);
+            isFresh = TRUE;
         }
     }
 
